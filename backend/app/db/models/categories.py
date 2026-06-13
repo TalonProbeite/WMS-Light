746b9e6ecd03b11f0035444c64b49d3b537b19fb
artem ponomarcev <siionunstoppable@gmail.com>
2026-06-13 12:58:19 +0300

    refactor(models): add indexes, enforce uniqueness, and set category_id to non-nullable

--- backend/app/db/models/categories.py
+++ backend/app/db/models/categories.py
@@ -6,8 +6,8 @@ class Categories(Base):
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
-    description = Column(String)
+    name = Column(String(100), nullable=False , unique=True)
+    description = Column(String , nullable=True)
 
     products = relationship("Products", back_populates="category")
 
